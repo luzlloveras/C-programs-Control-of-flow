47493b5c6186c6191d0e1f380b26054e0887ba3a
luzlloveras <luzlloveras7@gmail.com>
2020-10-15 22:41:03 -0300

    Update problema11.c

--- problema11.c
+++ problema11.c
@@ -3,24 +3,32 @@
 
 #include <stdio.h>
 
-int main() {
+int main() 
+{
 	int i, numero_previo, numero_actual, flag=0;
 	
 	printf("Ingrese numero\n");
 	scanf("%d", &numero_previo);
 	
-	for (i=0;i<10;i++) {
+	for (i = 0; i < 10; i++) 
+	{
 		printf("Ingrese numero\n");
 		scanf("%d", &numero_actual);
-		if (numero_previo>numero_actual)
+		if (numero_previo > numero_actual)
+		{
 			flag=1;
+		}
 	}
 	
-	if (flag==0)
+	if (flag == 0)
+	{
 		printf("La secuencia es creciente.\n");
+	}
 	else 
+	{
 		printf("La secuencia NO es creciente.\n");
-		
+	}
+	
 	return 0;
 }
 
