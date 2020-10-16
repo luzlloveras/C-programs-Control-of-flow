2083a40dada51ce633b79675e440cb92d7087c23
luzlloveras <luzlloveras7@gmail.com>
2020-10-15 22:38:12 -0300

    Update problema09.c

--- problema09.c
+++ problema09.c
@@ -2,24 +2,26 @@
 
 #include <stdio.h>
 
-int main() {
+int main() 
+{
 	
 	int num, a, b, c, i;
 	int flag = 0;
-	for (num=111; num<=999; num++) {
+	for (num = 111; num <= 999; num++) 
+	{	
+		c = num % 10;
+		b = (num / 10) % 10;
+		a = (num / 10) / 10;
 		
-		c=num%10;
-		b=(num/10)%10;
-		a=(num/10)/10;
-		
-		if (num== a*b*c){
+		if (num == a * b * c)
+		{
 			printf("%d es igual a la multiplicacion de sus valores\n", num);
 			flag = 1;
 		}
 	}
-	
-	if (flag==0)
+	if (flag == 0)
+	{
 		printf("No hay valores que cumplan con la condicion\n");
-	
+	}
 	return 0;
 }
