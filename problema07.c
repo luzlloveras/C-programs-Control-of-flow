2d1b386d3404780b303b96c1d9c86c815c529a20
luzlloveras <luzlloveras7@gmail.com>
2020-10-15 22:35:52 -0300

    Update problema07.c

--- problema07.c
+++ problema07.c
@@ -2,23 +2,28 @@
 
 #include <stdio.h>
 
-int main() {
+int main() 
+{
 	int n, i, j, acum_div, cont_perfectos;
-		cont_perfectos=0;
+		cont_perfectos = 0;
 		
-	for (n=1;cont_perfectos<5;n++){
-		acum_div=0;
-		for (i=1;i<n;i++){
-			if(n % i == 0){
-				acum_div=acum_div+i;
+	for (n = 1; cont_perfectos < 5; n++)
+	{
+		acum_div = 0;
+		for (i = 1; i < n; i++)
+		{
+			if (n % i == 0)
+			{
+				acum_div += i;
 			}
 		}
-		if (acum_div==n){
+		if (acum_div == n)
+		{
 			printf("%d es numero perfecto\n", n);
 			cont_perfectos++;
-		}
-			
-		if (n%20000==0){
+		}	
+		if (n % 20000 == 0)
+		{
 			printf("Espere mientras se encuentra el quinto numero perfecto.\n");
 		}
 	}
